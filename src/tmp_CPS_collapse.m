cf00ffa9747363dec00528b767c489dd778fa9ce
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-12-03 18:21:08 +0900

    "revised to alternate basis"

--- src/tmp_CPS_collapse.m
+++ src/tmp_CPS_collapse.m
@@ -1,5 +1,5 @@
-function M = tmp_CPS_collapse(M, print_log)
-% CPS = CPS_collapse(M,print_log) 
+function M = tmp_CPS_collapse(M, isodd, print_log)
+% CPS = CPS_collapse(M, isodd, print_log) 
 %
 % Obtain CPS by collapsing METTS; collapsed state is determined at each site randomly
 % 
@@ -12,7 +12,8 @@ function M = tmp_CPS_collapse(M, print_log)
 %   ---M{1}---*---M{2}---* ... *---M{end}---
 %       |          |                 |
 %       ^3         ^3                ^3
-%
+% isodd: [boolean] : if 1, collapse to Sx eigenstates, if 0, collapse to Sz
+% eigenstates.
 % 
 % print_log [boolean] : whether print time, used memory or not
 %
@@ -23,17 +24,27 @@ if print_log
     tobj = tic2;
 end
 
+[S,~] = getLocalSpace('Spin',1);
+
+if isodd
+    S = (S(:,:,1) + S(:,:,3))/sqrt(2);
+else 
+    S = S(:,:,2);
+end 
+
+[V,~] = eig(S);
 N = numel(M); % the number of sites
 dim = size(M{1}, 3); % dimension of local Hilbert space 
 C = cell(1,dim); % set for pure state projector
 
-prob = zeros(1,dim); % probability for each pure state
 % generate pure state projector
 for it = (1:dim)
-    null = zeros(1,dim);
-    null(1,it) = 1;
+    null = zeros(dim, dim);
+    null(:,it) = V(:,it); % project 
     C{it} = null;
 end
+
+prob = zeros(1,dim); % probability for each pure state
 
 [M,s,~] = canonForm(M,1,[],0); % site-canonical form with orthogonality center site 1
 M{1} = contract(M{1}, 3, 2, diag(s), 2, 1, [1 3 2]);
@@ -42,7 +53,7 @@ for it = (1:N)
     % calculate probability 
     for it2 = (1:dim)
         Tmp = contract(M{it}, 3, 3, C{it2}, 2, 2);
-        prob(it2) = contract(Tmp, 3, (1:3), conj(Tmp), 3,(1:3));
+        prob(it2) = contract(Tmp, 3, (1:3), conj(M{it}), 3,(1:3));
     end
     prob = prob./sum(prob, 'all'); %sum(prob, 'all') is 1 but divide with it to remove noise. 
     
@@ -60,7 +71,7 @@ for it = (1:N)
 
     % Collapse the state with C 
     % divide sqrt(prob(idx)) to ensure norm = 1 
-    M{it} = contract(M{it}, 3, 3, diag(C{idx}), 2, 2)./sqrt(prob(idx));
+    M{it} = contract(M{it}, 3, 3, C{idx}, 2, 2)./sqrt(prob(idx));
         
     % move on to the next site (it+1) via SVD
     if it<N
