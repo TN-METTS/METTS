47af83dda5205caa137c667c00fbc7e2120f91a2
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-12-04 14:59:36 +0900

    final version figure 6 and result

--- src/reproduce_figure6.m
+++ src/reproduce_figure6.m
@@ -11,7 +11,7 @@ SN =500; % sampling number
 print_log = false;
 
 % Imaginary time evolution parameters
-Nkeep = 20; % bond dimension
+Nkeep = 40; % bond dimension
 dt = 1/20; % discrete time step size
 
 % Local operators
@@ -54,6 +54,8 @@ H_MPO = MPO_canonForm(H_MPO, 0, [], 1e-8);
 fprintf("For N=%d, spin=%d, beta=%1.3f, dt=%0.4f, Heisenberg chain, calculation started\n", N, 1, beta, dt);
 
 Step_num = 10; 
+Ms=cell(1,SN);
+
 % case 1 
 z_only = zeros(SN, Step_num);
 for itS=(1:SN)
@@ -62,12 +64,16 @@ for itS=(1:SN)
         M{itN} = permute(rand(1, 3),[1 3 2]);
     end
     %normalize
-    M = canonForm(M, 0, [], []);
+    M = canonForm(M, 0, [], []);        
+    M = tmp_CPS_collapse(M, basis( :, :,2),  print_log);
+    Ms{itS}=M;
+
     for step= (1:Step_num)
         % Use CPS_collapse to generate orthonormal basis 
-        M = tmp_CPS_collapse(M, basis( :, :,2),  print_log);
         [M, isright] = TS_1D(M, H, Nkeep, dt, beta/2, print_log);
         z_only(itS, step) = exp_val(M, H_MPO);
+        M = tmp_CPS_collapse(M, basis( :, :,2),  print_log);
+
     end 
 end 
 z_only = z_only./N;
@@ -77,37 +83,43 @@ disptime('z only ended')
 % case 2 
 random = zeros(SN, Step_num);
 for itS=(1:SN)
-    M = cell(1,N);
-    for itN = (1:N)
-        M{itN} = permute(rand(1, 3),[1 3 2]);
-    end
+%     M = cell(1,N);
+%     for itN = (1:N)
+%         M{itN} = permute(rand(1, 3),[1 3 2]);
+%     end
     %normalize
-    M = canonForm(M, 0, [], []);
+%     M = canonForm(M, 0, [], []);
+    M=Ms{itS};
     for step= (1:Step_num)
         % Use CPS_collapse to generate orthonormal basis 
-        rand_basis=basis_random_axis(S);
-        M = tmp_CPS_collapse(M, rand_basis,  print_log);
         [M, isright] = TS_1D(M, H, Nkeep, dt, beta/2, print_log);
         random(itS, step) = exp_val(M, H_MPO);
+        rand_basis=basis_random_axis(S);
+        M = tmp_CPS_collapse(M, rand_basis,  print_log);
     end 
 end 
-random = random./N;
+random = real(random)./N;
 random_std= std(random, 0, 1);
 disptime('random ended')
 % case 3 
 max_mixed = zeros(SN, Step_num);
 for itS=(1:SN)
-    M = cell(1,N);
-    for itN = (1:N)
-        M{itN} = permute(rand(1, 3),[1 3 2]);
-    end
+%     M = cell(1,N);
+%     for itN = (1:N)
+%         M{itN} = permute(rand(1, 3),[1 3 2]);
+%     end
     %normalize
-    M = canonForm(M, 0, [], []);
+%     M = canonForm(M, 0, [], []);
+    M=Ms{itS};
+
+%     rand_basis=basis_random_axis(S);
+%     M = tmp_CPS_collapse(M, rand_basis,  print_log);
     for step= (1:Step_num)
         % Use CPS_collapse to generate orthonormal basis 
-        M = tmp_CPS_collapse(M, basis( :, :, mod(itS+1, 2)+1),  print_log);
         [M, isright] = TS_1D(M, H, Nkeep, dt, beta/2, print_log);
         max_mixed(itS, step) = exp_val(M, H_MPO);
+        M = tmp_CPS_collapse(M, basis( :, :, mod(step+1, 2)+1),  print_log);
+
     end 
 end 
 max_mixed = real(max_mixed)./N;
@@ -117,9 +129,9 @@ disptime('maximaly mixed ended')
 % plot the result 
 figure; 
 hold on ;
-errorbar((1:Step_num), mean(z_only, 1), z_only_std,'-s', 'MarkerSize',5,'MarkerEdgeColor','red','MarkerFaceColor','red', 'Color', 'red');
-errorbar((1:Step_num), mean(random, 1), random_std, '-o', 'MarkerSize',5,'MarkerEdgeColor','blue','MarkerFaceColor','blue', 'Color', 'blue');
-errorbar((1:Step_num), mean(max_mixed, 1), max_mixed_std, '-^', 'MarkerSize',5,'MarkerEdgeColor','green','MarkerFaceColor','green', 'Color', 'green');
+errorbar((1:Step_num), mean(z_only, 1), z_only_std./sqrt(SN),'-s', 'MarkerSize',5,'MarkerEdgeColor','red','MarkerFaceColor','red', 'Color', 'red');
+errorbar((1:Step_num), mean(random, 1), random_std./sqrt(SN), '-o', 'MarkerSize',5,'MarkerEdgeColor','blue','MarkerFaceColor','blue', 'Color', 'blue');
+errorbar((1:Step_num), mean(max_mixed, 1), max_mixed_std./sqrt(SN), '-^', 'MarkerSize',5,'MarkerEdgeColor','green','MarkerFaceColor','green', 'Color', 'green');
 hold off;
 set(gca, 'FontSize', 13, 'LineWidth', 1);
 legend({'Z Only', 'Random', 'Maximally Mixed'})
@@ -137,6 +149,10 @@ function rand_basis=basis_random_axis(S)
     dim =size(S, 3);
     axis = rand( dim, 1);
     axis = axis./norm(axis(:));
-    mat = contract(S, 3, 3, axis, 2, 1);
+    new_S=zeros(size(S));
+    new_S(:,:,3)=S(:,:,2);
+    new_S(:,:,1)=(S(:,:,1)+S(:,:,3))/sqrt(2);
+    new_S(:,:,2)=(S(:,:,1)-S(:,:,3))/sqrt(2)/1i;
+    mat = contract(new_S, 3, 3, axis, 2, 1);
     [rand_basis, ~] = eig((mat+mat')/2);
 end 
