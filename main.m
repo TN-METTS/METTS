626632917298a97882da50bfa9ec734a4d52883e
Minyoung-Kim1110 <kmmy1110@gmail.com>
2022-12-07 22:24:31 +0900

    final version of main.m

--- main.m
+++ main.m
@@ -1,9 +1,25 @@
 clear
 
+% Set path 
+PATH = 'C:/Users/IQSL/Documents/MATLAB/TN/FinalProject/result/';
+
+% To calculate, set isSaved = false
+isSaved = true; 
+
 % solve (c) : reproduce figure 6 
-%reproduce_figure6;
-%generate_figure6_from_saved_result;
+if ~isSaved 
+    reproduce_figure6;
+end 
+generate_figure6;
+
+%Calculate susceptibility and specific heat 
+if ~isSaved 
+    specific_heat; %should be revised 
+    susceptibility;
+
+end 
+
+% generate figure 
+generate_specific_heat_susceptibility_figure;
 
 
-
-susceptibility;
